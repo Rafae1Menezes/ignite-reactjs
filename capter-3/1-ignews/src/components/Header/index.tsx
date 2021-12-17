66a7251a713383393e43c5560797103baddb8c17
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-17 11:32:49 -0300

    cria home

--- capter-3/1-ignews/src/components/Header/index.tsx
+++ capter-3/1-ignews/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
    return (
       <header className={styles.headerContainer}>
          <div className={styles.headerContent}>
-            <Image src="/images/logo.svg" alt="ig.news" width={108} height={30} layout="intrinsic" />
+            <Image src="/images/logo.svg" alt="ig.news" width={108} height={30} />
             <nav>
                <a className={styles.active}>Home</a>
                <a>Posts</a>
--- capter-3/1-ignews/src/pages/index.tsx
+++ capter-3/1-ignews/src/pages/index.tsx
@@ -1,7 +1,23 @@
-
+import Image from 'next/image'
+import styles from './home.module.scss'
 
 export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+   return (
+      <>
+         <h1>Home | ig.news</h1>
+
+         <main className={styles.contentContainer}>
+            <section className={styles.hero}>
+               <span>👏 Hey, welcome</span>
+               <h1>News about the <span>React</span> world.</h1>
+               <p>
+                  Get acess to all the publications <br />
+                  <span>for $ 9.90</span>
+               </p>
+            </section>
+
+            <Image width={334} height={520} src="/images/avatar.svg" alt="Girl coding" />
+         </main>
+      </>
+   )
 }
