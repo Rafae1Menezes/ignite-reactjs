0132c8a9c057e4952e119877b3d07695f1ec198b
Rafael Menezes <menezes.inbox@gmail.com>
2021-12-22 23:33:12 -0300

    cria preview

--- capter-3/1-ignews/src/components/Header/index.tsx
+++ capter-3/1-ignews/src/components/Header/index.tsx
@@ -12,10 +12,10 @@ export function Header() {
    return (
       <header className={styles.headerContainer}>
          <div className={styles.headerContent}>
-            <Link href="/" passHref><Image src="/images/logo.svg" alt="ig.news" width={108} height={30} /></Link>
+            <Link href="/" passHref><a><Image src="/images/logo.svg" alt="ig.news" width={108} height={30} /></a></Link>
             <nav>
                <ActiveLInk activeClassName={styles.active}  href="/"><a>Home</a></ActiveLInk>
-               <ActiveLInk activeClassName={styles.active}  href="/posts" prefetch><a>Posts</a></ActiveLInk>
+               <ActiveLInk activeClassName={styles.active}  href="/posts"><a>Posts</a></ActiveLInk>
             </nav>
             <SignInButton />
          </div>
