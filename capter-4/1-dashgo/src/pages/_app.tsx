50d95c85397fe3eac151e1599f18e213b25cf29f
Rafael Menezes <menezes.inbox@gmail.com>
2022-01-24 20:44:22 -0300

    adiciona mirage e react-query

--- capter-4/1-dashgo/src/pages/_app.tsx
+++ capter-4/1-dashgo/src/pages/_app.tsx
@@ -2,14 +2,26 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import { SideBarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { theme } from '../styles/theme'
+import { makeServer } from '../services/mirage';
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+if (process.env.NODE_ENV === 'development') {
+   makeServer()
+}
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <ChakraProvider theme={theme}>
-         <SideBarDrawerProvider>
-            <Component {...pageProps} />
-         </SideBarDrawerProvider>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+         <ChakraProvider theme={theme}>
+            <SideBarDrawerProvider>
+               <Component {...pageProps} />
+            </SideBarDrawerProvider>
+         </ChakraProvider>
+         <ReactQueryDevtools />
+      </QueryClientProvider>
    )
 }
 
